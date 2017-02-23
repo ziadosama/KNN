1a7ad6da634d0020e887cc43acd2303be55f2d3b
Ziad Osama <ziadosama@users.noreply.github.com>
2017-02-23 17:20:49 +0200

    temp test

---
+++ NearestNeighbor.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from scipy.spatial import distance
+
+class NearestNeighbor(object):
+    # http://cs231n.github.io/classification/
+
+    def __init__(self):
+        pass
+
+    def train(self, xin, yin):
+        # the nearest neighbor classifier simply remembers all the training
+        # data
+        self.Xtr = xin
+        self.ytr = yin
+
+    def predict(self, X, k, l='L1'):
+        if l == 'L1':
+            ahmed = distance.cdist(X, self.Xtr, 'euclidean')
+        else:
+            # cityblock is the same as manhattan distance
+            ahmed = distance.cdist(X, self.Xtr, 'cityblock')
+        labels = np.zeros(len(X))
+        for i in range(len(labels)):
+            partition = np.argpartition(ahmed[i], k)[:k]
+            labels[i]=np.argmax(np.bincount(self.ytr[partition]))
+        return labels
---
+++ cifar10-knnL2.py
@@ -0,0 +1,50 @@
+
+# coding: utf-8
+
+#http://cs231n.github.io/classification/
+import random
+import math
+import numpy as np
+from data_utils import load_CIFAR10
+import matplotlib.pyplot as plt
+
+# Load the raw CIFAR-10 data.
+cifar10_dir = '/home/cse/Downloads/Untitled/cifar-10-batches-py'
+
+num_training = 10000
+num_test = 10000
+X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+
+# As a sanity check, we print out the size of the training and test data.
+print 'Training data shape: ', X_train.shape
+print 'Training labels shape: ', y_train.shape
+print 'Test data shape: ', X_test.shape
+print 'Test labels shape: ', y_test.shape
+
+
+# Subsample the data for more efficient code execution in this exercise
+mask = range(num_training)
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+mask = range(num_test)
+X_test = X_test[mask]
+y_test = y_test[mask]
+# Reshape the image data into rows
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
+X_test = np.reshape(X_test, (X_test.shape[0], -1))
+
+import NearestNeighbor as NN
+classifier = NN.NearestNeighbor()
+classifier.train(X_train, y_train)
+k=int(math.sqrt(num_training/num_test))
+
+ks =[3,4,5,7,8,9,15]
+
+for i in range(len(ks)):
+	print 'k= %d' % ks[i]
+	Yval_predict = classifier.predict(X_test, ks[i], 'L2')
+	num_correct = np.sum(Yval_predict == y_test)
+	accuracy = float(num_correct) / num_test
+	print 'Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy)
